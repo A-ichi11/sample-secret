e9b2cee0559d705c540abe7c3359d12c62ccecef
中嶋栄光 <e.nakashima@gemcook.com>
2021-11-09 18:11:39 +0900

    feat: secret

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/EikoNakashima/sample-secret
+
+go 1.17
+
+require (
+	github.com/aws/aws-sdk-go v1.42.0
+	github.com/go-sql-driver/mysql v1.6.0
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	awsSecretsManager "github.com/aws/aws-sdk-go/service/secretsmanager"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type secretsManager struct {
+	SecretsManager *awsSecretsManager.SecretsManager
+}
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(endpoints.ApNortheast1RegionID),
+		Credentials: credentials.NewStaticCredentials(
+			"AWSアクセスキー",
+			"AWSシークレットアクセスキー",
+			"",
+		),
+	})
+
+	secretsManager := &secretsManager{awsSecretsManager.New(sess)}
+	input := &awsSecretsManager.GetSecretValueInput{
+		SecretId: aws.String("gc-sun/dev/audit-resources/private-key"),
+	}
+	value, err := secretsManager.SecretsManager.GetSecretValue(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(value)
+}
